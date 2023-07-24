0bd54bbcc4a3659679ddbf76a5cf2687c6b032a4
Harikrishnan71 <111435574+Harikrishnan71@users.noreply.github.com>
2023-07-24 14:47:48 +0530

    Update stats-test.cpp-Changed Require to Expect

--- stats-test.cpp
+++ stats-test.cpp
@@ -16,9 +16,9 @@ TEST(Statistics, AverageNaNForEmpty) {
     //All fields of computedStats (average, max, min) must be
     //NAN (not-a-number), as defined in math.h
     
-    REQUIRE(std::isnan(computedStats.average));
-    REQUIRE(std::isnan(computedStats.max));
-    REQUIRE(std::isnan(computedStats.min));
+    EXPECT_TRUE(std::isnan(computedStats.average));
+    EXPECT_TRUE(std::isnan(computedStats.max));
+    EXPECT_TRUE(std::isnan(computedStats.min));
     
     //Design the REQUIRE statement here.
     //Use http://www.cplusplus.com/reference/cmath/isnan/
