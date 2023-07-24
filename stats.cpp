d4a44343c879a70882691ad2245ae0cc0ba29416
Harikrishnan71 <111435574+Harikrishnan71@users.noreply.github.com>
2023-07-24 11:33:07 +0530

    Update stats.cpp- Implementing the return values

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,23 @@
 #include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+Stats Statistics::ComputeStatistics(const std::vector<float>& value) {
     //Implement statistics here
+    Stats x;
+    if (value.size()==0)
+    {
+        x.average =NAN;
+        x.max =NAN;
+        x.min =NAN;
+    }
+    else
+    {
+        x.max = *max_element(value.begin(),value.end());
+        x.min = *max_element(value.begin(),value.end());
+        x.average = (accumulate(value.begin(),value.end(),0))/value.size();
+    
+    }
+    return x;
 }
+        
+        
+
